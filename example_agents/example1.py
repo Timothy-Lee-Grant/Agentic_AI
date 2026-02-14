d8db7ae49cc041a2ddff45d0f310a66279f9f0d5
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-02-14 15:05:30 -0800

    Adding extra example, and set up features.

--- example_agents/example1.py
+++ example_agents/example1.py
@@ -9,14 +9,15 @@ ram_info = get_ram_usage()
 
 messages = [
     {
-        'role': 'user',
-        'content': f"The user's computer has the following RAM stats: {ram_info}. Explain the computer's health"
-    },
-    {
         'role': 'system',
         'content': "You are a literal hardware monitor. Do not calculate new numbers. Only use the numbers provided in the user prompt. Be extremely concise."
+    },
+    {
+        'role': 'user',
+        'content': "The user's computer has the following RAM stats:. Explain the computer's health."
     }
 ]
+
 
 response = ollama.chat(model='qwen2.5:0.5b', messages=messages)
 print(response['message']['content'])
