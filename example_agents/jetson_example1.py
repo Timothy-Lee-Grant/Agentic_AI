582e485583c13920d264a2b56248a6aca75c91a1
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-02-21 15:50:51 -0800

    Changed jetson_example.py to be able to correctly use tools. There are still some errors in this code, need to keep troubleshooting.

--- example_agents/jetson_example1.py
+++ example_agents/jetson_example1.py
@@ -1,48 +1,41 @@
 import ollama
+#response: ChatResponse = ollama.chat(...)
 
 def StatusOfLightsInRoom():
+    """Returns the state of all the lights in the user's room."""
     return "All lights are currently on."
 
 def TemperatureInRoom():
+    """Returns the temperature inside the user's room."""
     return "20 degrees Celcius."
 
-StatusOfLightsInRoomPtr = StatusOfLightsInRoom()
-TemperatureInRoomPtr = TemperatureInRoom()
+available_functions = {
+    "StatusOfLightsInRoom": StatusOfLightsInRoom,
+    "TemperatureInRoom": TemperatureInRoom
+}
 
-messages = [
-    {
-        "role": "system",
-        "content": """You are an AI assistant that can request tools when needed"
-            AVAILABLE TOOLS:
-            - StatusOfLightsInRoom : Returns the state of all the lights in the user's room.
-                Format: {"tool": "StatusOfLightsInRoom", "args": ""}
-            - TemperatureInRoom : Returns the temperature inside the user's room.
-                Format: {"tool": "TemperatureInRoom", "args": ""}
+messages = [{"role": "system", "content":"You are a smart home assistant"}]
 
-            RULES:
-            1. You may only request tools which are provided in the AVAILABLE TOOLS list
-            2. If no tool is needed answer normally.
-            """
-    },
-    {
-        "role": "user",
-        "content": ""
-    }
-]
 
 while True:
     userInput = input(">>>")
-    if userInput == 'q' or userInput == 'quit':
-        exit()
-    messages[1]["content"] = userInput
-    response = ollama.chat(model = 'llama3:8b', messages=messages)
-    if response['message']['content'] == '{"tool": "StatusOfLightsInRoom", "args": ""}':
-        messages.append({"role":"tool", "content": {StatusOfLightsInRoomPtr}})
-        response = ollama.chat(model='llama3:8b', messages=messages)
-        print(response['message']['content'])
-    elif response['message']['content'] == '{"tool": "TemperatureInRoom", "args": ""}':
-        messages.append({"role":"tool", "content": {TemperatureInRoomPtr}})
-        response = ollama.chat(model='llama3:8b', messages=messages)
-        print(response['message']['content'])
-    else:
-        print(response['message']['content'])
+    if userInput.lower() in ['q', 'quit']: break
+
+    messages.append({"role": "user", "content": userInput})
+
+
+    response = ollama.chat(model='llama3.1:8b', messages=messages, tools=available_functions)
+    messages.append(response.message)
+    
+    for tool in response.message.tool_calls or []:
+        print(f"---- Thinking: I need to use {tool.function.name} ----")
+        functionToCall = available_functions[tool.function.name]
+        toolOutput = functionToCall()
+
+        messages.append({"role": "tool", "content": toolOutput})
+
+    response = ollama.chat(model='llama3.1:8b', messages=messages)
+    print(response.message.content)
+    messages.append({"role": "assistant", "content": response.message.content})
+
+
