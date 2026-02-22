d45eb85870443e335a58ef63d9befffe70e4e5b7
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-02-21 16:25:31 -0800

    Changed the system prompt to allow llm to chat instead of just being a smart assistant tool user. But the model still keeps trying to use tools even when no tool would be required. It also calls for those tools incorrectly, by outputting json into the text content instead of the tool_calls list. It also is making up it own tools (which I didn't tell it that it has.

--- example_agents/jetson_example1.py
+++ example_agents/jetson_example1.py
@@ -2,11 +2,11 @@ import ollama
 #response: ChatResponse = ollama.chat(...)
 
 def StatusOfLightsInRoom():
-    """Returns the state of all the lights in the user's room."""
+    """Returns the state of all the lights in the user's room. Takes no arguments"""
     return "All lights are currently on."
 
 def TemperatureInRoom():
-    """Returns the temperature inside the user's room."""
+    """Returns the temperature inside the user's room. Takes no arguments"""
     return "20 degrees Celcius."
 
 available_functions = {
@@ -14,7 +14,9 @@ available_functions = {
     "TemperatureInRoom": TemperatureInRoom
 }
 
-messages = [{"role": "system", "content":"You are a smart home assistant"}]
+available_function_ptr = [StatusOfLightsInRoom, TemperatureInRoom]
+#messages = [{"role": "system", "content": "You are a smart home assistant. Use tools only when necessary. If you use a tool, do not talk about the JSON—just use the tool."}]
+messages = [{"role": "system", "content": "You are a intelligent chatbot. You also have access to tool if you find them nessissary during conversations."}]
 
 
 while True:
@@ -24,18 +26,21 @@ while True:
     messages.append({"role": "user", "content": userInput})
 
 
-    response = ollama.chat(model='llama3.1:8b', messages=messages, tools=available_functions)
+    response = ollama.chat(model='llama3.1:8b', messages=messages, tools=available_function_ptr, keep_alive=-1)
     messages.append(response.message)
-    
-    for tool in response.message.tool_calls or []:
-        print(f"---- Thinking: I need to use {tool.function.name} ----")
-        functionToCall = available_functions[tool.function.name]
-        toolOutput = functionToCall()
+    if response.message.tool_calls:
+        for tool in response.message.tool_calls:
+            print(f"---- Thinking: I need to use {tool.function.name} ----")
+            functionToCall = available_functions[tool.function.name]
+            toolOutput = functionToCall()
 
-        messages.append({"role": "tool", "content": toolOutput})
+            messages.append({"role": "tool", "content": toolOutput, "name":tool.function.name})
 
-    response = ollama.chat(model='llama3.1:8b', messages=messages)
-    print(response.message.content)
-    messages.append({"role": "assistant", "content": response.message.content})
+        response = ollama.chat(model='llama3.1:8b', messages=messages)
+        print(response.message.content)
+        #messages.append({"role": "assistant", "content": response.message.content})
+        messages.append(response.message)
+    else:
+        print(response.message.content)
 
 
