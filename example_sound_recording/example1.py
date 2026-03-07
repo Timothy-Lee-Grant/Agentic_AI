c37be189559398236a6843a4cba883e725ac6cc7
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-03-07 14:36:21 -0800

    Adding examples for how to capture and process audio from the Nvidia Nano with an attached speaker/microphone combo.

---
+++ example_sound_recording/example1.py
@@ -0,0 +1,51 @@
+import pyaudio
+import wave
+import subprocess
+import os
+
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = 1024
+WAVE_OUTPUT = "input.wav"
+WHISPER_PATH = "/home/timothy/Desktop/whisper.cpp/build/bin/whisper-cli"
+MODEL_PATH = "/home/timothy/Desktop/whisper.cpp/models/ggml-base.bin"
+ 
+def record_audio():
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                        input=True, input_device_index=2 , frames_per_buffer=CHUNK)
+    
+    print("Listening")
+    frames = []
+
+    try:
+        while True:
+            data = stream.read(CHUNK)
+            frames.append(data)
+    except KeyboardInterrupt:
+        print("Finishing")
+    
+    with wave.open(WAVE_OUTPUT, 'wb') as wf:
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(audio.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+    
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+def transcribe():
+    #call the C++ binary directly
+    # -nt: no time stamps, -otxt: ouput just the text
+    cmd = [WHISPER_PATH, "-m", MODEL_PATH, "-f", WAVE_OUTPUT, "-nt"]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    return result.stdout.strip()
+
+if __name__ == "__main__":
+    record_audio()
+    text = transcribe()
+    print(f"you said {text}") 
