b9ae18c08da924d4960b3c89d8628032d6347e58
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-03-21 15:00:13 -0700

    Successfully ran the audio transcription Python to be able to get a string of text to be transcribed and sent to the speaker performing text-to-speech.

--- example_audio_operations/audio_transcription_example.py
+++ example_audio_operations/audio_transcription_example.py
@@ -5,12 +5,16 @@ import subprocess
 
 
 
-PIPER_PATH = "./piper/piper"
-MODEL_PATH =  "en_US-lessac-medium.onnx"
+#PIPER_PATH = "./piper/piper"
+PIPER_PATH = "/home/timothy/Desktop/Agentic_AI/sound_processing/piper/piper"
+MODEL_PATH =  "/home/timothy/Desktop/Agentic_AI/sound_processing/en_US-lessac-medium.onnx"
 
 def speak(text):
     print(f"Speaking {text}")
 
-    command = f'echo "{text}" | {PIPER_PATH} --model {MODEL_PATH} --output_raw | aplay -r 22050 -f S16_LE -t raw -D hw:0,0'
+    command = f'echo "{text}" | {PIPER_PATH} --model {MODEL_PATH} --output_raw | aplay -r 22050 -f S16_LE -t raw -c 1 -D plughw:0,0'
 
     subprocess.run(command, shell=True)
+
+example_text = "Hello, my name is Timothy Grant"
+speak(example_text)
